fe5765e6b2288c7b81fe91b3fb61c18dff2f3448
decagon <decagon@decagons-MacBook-Air.local>
2022-11-30 16:25:57 +0100

    todo complete

---
+++ src/routes/todoRoute.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+import { createTodo, getTodo, updateTodo, removeTodo} from '../controller/todoController'
+
+const router = express.Router();
+
+router.get('/get-todo', getTodo)
+router.post('/create', createTodo)
+router.patch("/update-todo/:id", updateTodo)
+router.delete("/remove-todo/:id", removeTodo)
+
+export default router
